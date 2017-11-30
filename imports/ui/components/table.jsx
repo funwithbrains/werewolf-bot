e14d053d8d83cf74384bdf07a1d71ddf92121c6a
Cory <cory.andrew.1988@gmail.com>
2017-11-30 02:27:41 -0500

    added a link option to the table component

--- imports/ui/components/table.jsx
+++ imports/ui/components/table.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
+import { Link } from 'react-router-dom';
 
 export default withTracker(({ collection, query }) => {
   return {
@@ -7,17 +8,22 @@ export default withTracker(({ collection, query }) => {
   };
 })(class extends Component {
   render() {
-    const { values, definition: { columns } } = this.props;
+    const { values, link, definition: { columns } } = this.props;
     
     return <table>
       <thead>
         <tr>
-          {columns.map(column => <th>{column.label || column.key}</th>)}
+          {columns.map(column => <th key={column.key}>{column.label || column.key}</th>)}
         </tr>
       </thead>
       <tbody>
         {values.map(value => <tr key={value._id}>
-          {columns.map(column => <td>{value[column.key]}</td>)}
+          {columns.map(column => <td key={column.key}>
+            {column.isLink ?
+              <Link to={link.replace(':id', value._id)}>{value[column.key]}</Link> :
+              value[column.key]
+            }
+          </td>)}
         </tr>)}
       </tbody>
     </table>;
