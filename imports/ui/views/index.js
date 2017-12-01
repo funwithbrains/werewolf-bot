0f881bd8044a6a808818ec45e74ac16c6a92828c
Cory <cory.andrew.1988@gmail.com>
2017-12-01 01:55:11 -0500

    applied the CRUD component to simplify the roles views

--- imports/ui/views/index.js
+++ imports/ui/views/index.js
@@ -1,6 +1,6 @@
 import Home from './home.jsx';
 import Players from './players.jsx';
-import Roles from './roles/index.jsx';
+import Roles from './roles.jsx';
 import Setup from './setup.jsx';
 
 export {
---
+++ imports/ui/views/roles.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Roles } from '/imports/api/index';
+import { CRUD } from '/imports/ui/components/index';
+
+export default () => <div>
+  <CRUD
+    basePath="/roles"
+    nameSingular="Role"
+    namePlural="Roles"
+    collection={Roles}
+    listColumns={[
+      { label: 'Name', key: 'name', isLink: true },
+      { label: 'Description', key: 'description' }
+    ]}
+    formFields={[
+      { key: 'name', type: 'text' },
+      { key: 'description', type: 'longText' },
+      { key: 'tags', type: 'text' }
+    ]}
+  />
+</div>;
