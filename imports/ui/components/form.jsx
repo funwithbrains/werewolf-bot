b4ee7abca58a84dbd39c169863e354a99dfec527
Cory Watson <cryo.warden@gmail.com>
2017-12-06 02:07:55 -0500

    made forms use the new wrapped inputs

--- imports/ui/components/form.jsx
+++ imports/ui/components/form.jsx
@@ -1,53 +1,34 @@
 import React, { Component } from 'react';
 
-import { DateTime } from '/imports/ui/components/input/index';
+import { DateTime, Input } from '/imports/ui/components/input/index';
 
-const missingComponent = () => <div>Missing Component</div>;
+const FormInput = ({ type, value, onChange }) => {
+  if (type === 'longText') {
+    return <Input value={value} onChange={onChange} type="long-text" />
+  }
+  
+  if (type === 'dateTime') {
+    return <DateTime value={value} onChange={onChange} />;
+  }
 
-const fieldTypeMap = Object.freeze({
-  'text': key => <input ref={key} type="text" />,
-  'longText': key => <textarea ref={key}></textarea>,
-  'dateTime': key => <DateTime ref={key} />
-});
+  return <Input value={value} onChange={onChange} type={type} />;
+};
 
 export default class extends Component {
-  componentDidMount() {
-    this.updateInputValues(this.props);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.updateInputValues(nextProps);
-  }
-
-  updateInputValues({ value }) {
-    if (!value) { return; }
-
-    Object.keys(this.refs).forEach(key => {
-      this.refs[key].value = value[key];
-    });
-  }
-
-  save() {
-    const { handleSave } = this.props;
-
-    const value = Object.keys(this.refs).reduce((memo, key) => {
-      memo[key] = this.refs[key].value;
-      return memo;
-    }, {});
-
-    handleSave(value);
-  }
-
   render() {
-    const { fields } = this.props;
-    
+    const { value: values, fields, onChange } = this.props;
+
     return <div>
       {fields.map(field => <div key={field.key}>
-        <label>{field.label || field.key}</label> {
-          (fieldTypeMap[field.type] || missingComponent)(field.key)
-        }
+        <label>{field.label || field.key}</label>
+        <span>
+          <FormInput
+            type={field.type}
+            value={values && values[field.key] || ''}
+            onChange={v => onChange(field.key, v)}
+          />
+        </span>
       </div>)}
-      <button onClick={() => this.save()}>Save</button>
     </div>;
   }
 };
