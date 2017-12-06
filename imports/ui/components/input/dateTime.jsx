3e85146c4121ead0c743674181a255a3c920ea9d
Cory Watson <cryo.warden@gmail.com>
2017-12-06 02:07:18 -0500

    made the DateTime input act like the input wrappers

--- imports/ui/components/input/dateTime.jsx
+++ imports/ui/components/input/dateTime.jsx
@@ -1,12 +1,64 @@
 import React, { Component } from 'react';
+
 import { moment, dateTime } from '/imports/utils/index';
 
+const parseMomentValue = string => string ? dateTime.parse(string) : moment();
+const createMomentValue = date => date ? moment(date) : moment();
+
 export default class extends Component {
-  handleChange() {
-    console.log(dateTime.format(dateTime.parse(this.refs.input.value)));
+  constructor(props) {
+    super(props);
+
+    const { value } = props;
+    const momentValue = createMomentValue(value);
+
+    this.state = {
+      inputString: dateTime.format(momentValue)
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { value } = nextProps;
+    const momentValue = createMomentValue(value);
+    
+    this.setState({
+      inputString: dateTime.format(momentValue)
+    });
+  }
+
+  handleChange(event) {
+    const inputString = event.target.value;
+
+    this.setState({ inputString });
+
+    const { onChange } = this.props;
+    if (onChange) {
+      const momentValue = parseMomentValue(inputString);
+      if (momentValue.isValid()) {
+        onChange(momentValue.toDate());
+      }
+    }
   }
 
   render() {
-    return <input ref="input" type="text" onChange={() => this.handleChange()} />
+    const { value } = this.props;
+    const momentValue = createMomentValue(value);
+    const isValid = momentValue.isValid();
+
+    const { inputString } = this.state;
+    
+    return <span>
+      <input
+        type="text"
+        value={inputString}
+        onChange={e => this.handleChange(e)}
+      />
+      {isValid ?
+        null :
+        <span> not valid, using previous value: </span>
+      }
+      <span> {momentValue.fromNow()}</span>
+      <span> {dateTime.format(momentValue)}</span>
+    </span>;
   }
 };
