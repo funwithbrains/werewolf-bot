300d0666dfa1354948d1543058dbdbfcc16a3e2d
Cory Watson <cryo.warden@gmail.com>
2017-12-07 03:04:31 -0500

    added some fields to the game editor

--- imports/ui/views/games.jsx
+++ imports/ui/views/games.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Games } from '/imports/api/index';
+import { Games, Players } from '/imports/api/index';
 import { CRUD } from '/imports/ui/components/index';
 
 export default () => <div>
@@ -13,8 +13,22 @@ export default () => <div>
       { label: 'Name', key: 'name', isLink: true }
     ]}
     formFields={[
-      { key: 'name', type: 'text' },
-      { key: 'startDate', type: 'dateTime' }
+      { key: 'name', type: 'text', label: 'Name' },
+      { key: 'startDate', type: 'dateTime', label: 'Start Date' },
+      {
+        key: 'gameMasterId',
+        type: 'objectSelector',
+        label: 'Game Master',
+        collection: Meteor.users,
+        getLabel: v => v.emails[0].address
+      },
+      {
+        key: 'playerIds',
+        type: 'objectMultiSelector',
+        label: 'Players',
+        collection: Players,
+        getLabel: v => v.name
+      }
     ]}
     computeFormValue={formData => formData}
   />
